9733da68bf54beba375ac5f322834887e7259e6b
a22007432 <ssm.antonio@gmail.com>
2024-02-21 17:41:26 +0000

    Added ints

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //float numerofloat = 9.123f;
+            //string frase = "this string lol";
+            int numeroint = -9;
+            uint unsnumeroint = 9u;
+
+
+            //Console.WriteLine(numerofloat);
+            //Console.WriteLine(frase);
+            Console.WriteLine(numeroint);
+            Console.WriteLine(unsnumeroint);
+
+
+        }
+    }
+}
