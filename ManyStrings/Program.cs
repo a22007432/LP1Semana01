99c4d5922a6e719570b12dbcbff0f140717fe70b
a22007432 <ssm.antonio@gmail.com>
2024-02-22 14:48:26 +0000

    added ex 6

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -6,6 +6,10 @@ namespace ManyStrings
     {
         static void Main(string[] args)
         {
+            double xx = 1.23456;
+            int ii = 19;
+
+
             string s1 ="Isto é um string wow \u058D\u058D\u058D\u058D";
             string s2 ="Isto é outro string \u1368";
             string s3 ="Olha mais um, mas este é triste";
@@ -21,6 +25,19 @@ namespace ManyStrings
 
             Console.WriteLine(s4);
             Console.WriteLine(s5);
+
+            string s6 = $"xx={xx,2:f}";
+            string s7 = $"xx={xx,1:p}";
+            //n percebo
+
+            Console.WriteLine(s6);
+            Console.WriteLine(s7);
+
+            string s8 = $"ii={ii:x}";
+            string s9 = $"ii={ii:c}";
+            Console.WriteLine(s8);
+            Console.WriteLine(s9);
         }
+
     }
 }
