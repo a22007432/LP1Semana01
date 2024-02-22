05600e921b85d4d014a20129b512c4f1a03a0749
a22007432 <ssm.antonio@gmail.com>
2024-02-22 13:55:32 +0000

    added new project

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ManyStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
