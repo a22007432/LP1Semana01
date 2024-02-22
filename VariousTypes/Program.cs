eba474cfb3edd55f26b4794b0ade26c9e447b755
a22007432 <ssm.antonio@gmail.com>
2024-02-22 13:12:25 +0000

    added chars wiht unicodes

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace VariousTypes
 {
@@ -6,17 +7,27 @@ namespace VariousTypes
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8; 
             //float numerofloat = 9.123f;
             //string frase = "this string lol";
             int numeroint = -9;
             uint unsnumeroint = 9u;
-
 
             //Console.WriteLine(numerofloat);
             //Console.WriteLine(frase);
             Console.WriteLine(numeroint);
             Console.WriteLine(unsnumeroint);
 
+            //char c1 = '\U+27B3';
+            char c1 = '\u00A9';
+            char c2 = '\u25C6';
+            char c3 = '\u229A';
+            
+            
+            Console.WriteLine(c1);
+            Console.WriteLine(c2);
+            Console.WriteLine(c3);
+            
 
         }
     }
