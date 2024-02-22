d944a6de459452779073b473daa2b876bd82f32f
a22007432 <ssm.antonio@gmail.com>
2024-02-22 14:28:02 +0000

    added strings to MnyStrings :)

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -6,7 +6,13 @@ namespace ManyStrings
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string s1 ="Isto é um string wow \u058D\u058D\u058D\u058D";
+            string s2 ="Isto é outro string \u1368";
+            string s3 ="Olha mais um, mas este é triste";
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine(s3);
         }
     }
 }
