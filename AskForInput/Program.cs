ac010aa37147565757c4aad4210d4f5df712900a
a22007432 <ssm.antonio@gmail.com>
2024-02-23 14:02:38 +0000

    added ex7

--- AskForInput/Program.cs
+++ AskForInput/Program.cs
@@ -6,7 +6,20 @@ namespace AskForInput
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Pls  write an integer");
+            string answer1 = Console.ReadLine();
+
+            int i = int.Parse(answer1);
+            Console.WriteLine($"this is in i:-> {i}");
+            
+            Console.WriteLine("Pls  write a real number");
+            string answer2 = Console.ReadLine();
+
+            float f = float.Parse(answer2);
+
+            var result = i + f;
+            Console.WriteLine($"Here's the result->{result}");
+
         }
     }
 }
